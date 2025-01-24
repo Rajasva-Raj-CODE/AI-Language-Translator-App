867856b37bf00b72caa3f12493252f23ad8506bd
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-01-24 21:14:50 +0530

    voice recorder

--- src/app/components/voice-recorder.js
+++ src/app/components/voice-recorder.js
@@ -6,23 +6,72 @@ export default function VoiceRecorder() {
   const [audioBase64, setAudioBase64] = useState(null);
   const mediaRecorderRef = useRef(null);
   const chunksRef = useRef([]);
-  const toogleRecording = () => {
-    setIsRecording((prevIsRecording) => !prevIsRecording);
+
+  const startRecording = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+      mediaRecorderRef.current = new MediaRecorder(stream);
+      mediaRecorderRef.current.ondataavailable = (e) => {
+        if (e.data.size > 0) {
+          chunksRef.current.push(e.data);
+        }
+      };
+      mediaRecorderRef.current.onstop = () => {
+        const audioBlob = new Blob(chunksRef.current, {
+          type: "audio/wav",
+        });
+        const reader = new FileReader();
+        reader.readAsDataURL(audioBlob);
+        reader.onloadend = () => {
+          const base64Audio = reader.result;
+          setAudioBase64(base64Audio.split(",")[1]);
+        };
+        chunksRef.current = [];
+      };
+      mediaRecorderRef.current.start();
+      setIsRecording(true);
+    } catch (error) {
+      console.log("Error accessing microphone", error);
+    }
+
+    const stopRecording = () => {
+      if (mediaRecorderRef.current && isRecording) {
+        mediaRecorderRef.current.stop();
+        setIsRecording(false);
+      }
+    };
+
+    const toogleRecording = () => {
+      if (isRecording) {
+        stopRecording();
+      } else {
+        startRecording();
+      }
+    };
+
+    return (
+      <div className="flex items-center p-4">
+        <button
+          onClick={toogleRecording}
+          className={`w-12 h-12  rounded-full border border-gray-500 flex items-center justify-center ${
+            isRecording ? "bg-red-500 text-white" : ""
+          }`}
+        >
+          {isRecording ? (
+            <Square className="w-4 h-4" />
+          ) : (
+            <Mic className="w-4 h-4" />
+          )}
+        </button>
+        <input
+          type="hidden"
+          name="audio"
+          value={audioBase64 || ""}
+          aria-label="Recorded Audio"
+        />
+      </div>
+    );
   };
-  return (
-    <div className="flex items-center p-4">
-      <button
-        onClick={toogleRecording}
-        className={`w-12 h-12  rounded-full border border-gray-500 flex items-center justify-center ${
-          isRecording ? "bg-red-500 text-white" : ""
-        }`}
-      >
-        {isRecording ? (
-          <Square className="w-4 h-4" />
-        ) : (
-          <Mic className="w-4 h-4" />
-        )}
-      </button>
-    </div>
-  );
 }
