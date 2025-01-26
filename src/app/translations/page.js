311219b2dc86b636b9559892fa6e9fff31bb8d08
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-01-26 21:12:31 +0530

    voice recorder

--- src/app/translations/page.js
+++ src/app/translations/page.js
@@ -3,7 +3,7 @@ import { neon } from '@neondatabase/serverless';
 async function getData() {
   const sql = neon(process.env.DATABASE_URL);
   const response = await sql`SELECT version()`;
-  return response[0].version;
+  return response[0].version
 }
 
 export default async function Page() {
